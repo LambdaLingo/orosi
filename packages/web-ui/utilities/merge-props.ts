b102c093a7c2fd6f624862b16014bf3bb80cd82c
LambdaLingo <abdollahkhani@outlook.com>
2024-01-06 08:35:12 +0330

    Refactor merge-props.ts to handle null and undefined values more accurately

--- packages/web-ui/utilities/merge-props.ts
+++ packages/web-ui/utilities/merge-props.ts
@@ -6,10 +6,10 @@ type Props = Record<string, unknown>;
 type PropsArg = Props | null | undefined;
 
 // taken from: https://stackoverflow.com/questions/51603250/typescript-3-parameter-list-intersection-type/51604379#51604379
-type TupleTypes<T> = { [P in keyof T]: T[P] } extends { [key: number]: infer V }
+type TupleTypes<T> = { [P in keyof T]: T[P] } extends Record<number, infer V>
   ? NullToObject<V>
   : never;
-type NullToObject<T> = T extends null | undefined ? {} : T;
+type NullToObject<T> = T extends null | undefined ? Record<string, never> : T;
 type UnionToIntersection<U> = (
   U extends unknown ? (k: U) => void : never
 ) extends (k: infer I) => void
