24f4fa7cd7359c09f480e0b403895e64d9a912d2
LambdaLingo <abdollahkhani@outlook.com>
2024-01-07 23:51:20 +0330

    Refactor merge-props.ts to use TupleToUnion type

--- packages/web-ui/utilities/merge-props.ts
+++ packages/web-ui/utilities/merge-props.ts
@@ -6,7 +6,7 @@ type Props = Record<string, unknown>;
 type PropsArg = Props | null | undefined;
 
 // taken from: https://stackoverflow.com/questions/51603250/typescript-3-parameter-list-intersection-type/51604379#51604379
-type TupleTypes<T> = { [P in keyof T]: T[P] } extends { [key: number]: infer V }
+type TupleToUnion<T> = { [P in keyof T]: T[P] } extends Record<number, infer V>
   ? NullToObject<V>
   : never;
 type NullToObject<T> = T extends null | undefined ? {} : T;
