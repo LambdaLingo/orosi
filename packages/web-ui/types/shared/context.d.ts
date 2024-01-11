eb38834ddded6507aff3e4436eada7dac64c5ce1
LambdaLingo <abdollahkhani@outlook.com>
2024-01-11 17:45:54 +0330

    Refactor SlottedValue interface in context.d.ts

--- packages/web-ui/types/shared/context.d.ts
+++ packages/web-ui/types/shared/context.d.ts
@@ -1,9 +1,9 @@
 import type { WithRef } from "./ref";
 
-interface SlottedValue<T> {
+type SlottedValue<T> = {
   slots?: Record<string | symbol, T>;
   [slotCallbackSymbol]?: (value: T) => void;
-}
+};
 export type SlottedContextValue<T> = SlottedValue<T> | T | null | undefined;
 export type ContextValue<T, E extends Element> = SlottedContextValue<
   WithRef<T, E>
