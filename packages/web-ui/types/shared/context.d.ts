34aa17f2a74cd7d3b6c40fd91817383ee4584337
LambdaLingo <abdollahkhani@outlook.com>
2024-01-13 22:40:12 +0330

    Add WithRef type to ContextValue

--- packages/web-ui/types/shared/context.d.ts
+++ packages/web-ui/types/shared/context.d.ts
@@ -1,11 +1,13 @@
+import type { WithRef } from "./ref";
+
 type SlottedValue<T> = {
   slots?: Record<string | symbol, T>;
   [slotCallbackSymbol]?: (value: T) => void;
 };
 export type SlottedContextValue<T> = SlottedValue<T> | T | null | undefined;
-export type ContextValue<T, E extends Element> = SlottedContextValue<T> & {
-  ref?: ForwardedRef<E>;
-};
+export type ContextValue<T, E extends Element> = SlottedContextValue<
+  WithRef<T, E>
+>;
 export type SlotProps = {
   /**
    * A slot name for the component. Slots allow the component to receive props from a parent component.
